c8fe33734194f21d8bf329b55d8acc0cfc5b0ada
Daniel Mathiesen <daniel.mathiesen@gmail.com>
2014-06-09 17:46:20 +0200

    Added arrow icon

--- wp-content/themes/hioa/functions.php
+++ wp-content/themes/hioa/functions.php
@@ -5,7 +5,7 @@ add_action( 'wp_ajax_nopriv_RegisterSubscriber', 'RegisterSubscriber' );
 add_action( 'wp_ajax_RegisterSubscriber', 'RegisterSubscriber' );
 
 function RegisterSubscriber(){
-	// wp_mail("daniel.mathiesen@gmail.com", "viten+praksis", $_POST["email"]);
+	wp_mail("daniel.mathiesen@gmail.com", "viten+praksis", $_POST["email"]);
 	die("1");
 }
 
