886d06bb4d75524dd743f30ed83a9a763c2e5f51
Daniel Mathiesen <daniel.mathiesen@gmail.com>
2014-06-10 09:53:11 +0200

    Fixed css rendering bug with js

--- wp-content/themes/hioa/footer.php
+++ wp-content/themes/hioa/footer.php
@@ -138,21 +138,14 @@
 			(function(t){"use strict";function e(t,e,r){return t.addEventListener?t.addEventListener(e,r,!1):t.attachEvent?t.attachEvent("on"+e,r):void 0}function r(t,e){var r,n;for(r=0,n=t.length;n>r;r++)if(t[r]===e)return!0;return!1}function n(t,e){var r;t.createTextRange?(r=t.createTextRange(),r.move("character",e),r.select()):t.selectionStart&&(t.focus(),t.setSelectionRange(e,e))}function a(t,e){try{return t.type=e,!0}catch(r){return!1}}t.Placeholders={Utils:{addEventListener:e,inArray:r,moveCaret:n,changeType:a}}})(this),function(t){"use strict";function e(){}function r(){try{return document.activeElement}catch(t){}}function n(t,e){var r,n,a=!!e&&t.value!==e,u=t.value===t.getAttribute(V);return(a||u)&&"true"===t.getAttribute(D)?(t.removeAttribute(D),t.value=t.value.replace(t.getAttribute(V),""),t.className=t.className.replace(R,""),n=t.getAttribute(F),parseInt(n,10)>=0&&(t.setAttribute("maxLength",n),t.removeAttribute(F)),r=t.getAttribute(P),r&&(t.type=r),!0):!1}function a(t){var e,r,n=t.getAttribute(V);return""===t.value&&n?(t.setAttribute(D,"true"),t.value=n,t.className+=" "+I,r=t.getAttribute(F),r||(t.setAttribute(F,t.maxLength),t.removeAttribute("maxLength")),e=t.getAttribute(P),e?t.type="text":"password"===t.type&&M.changeType(t,"text")&&t.setAttribute(P,"password"),!0):!1}function u(t,e){var r,n,a,u,i,l,o;if(t&&t.getAttribute(V))e(t);else for(a=t?t.getElementsByTagName("input"):b,u=t?t.getElementsByTagName("textarea"):f,r=a?a.length:0,n=u?u.length:0,o=0,l=r+n;l>o;o++)i=r>o?a[o]:u[o-r],e(i)}function i(t){u(t,n)}function l(t){u(t,a)}function o(t){return function(){m&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)?M.moveCaret(t,0):n(t)}}function c(t){return function(){a(t)}}function s(t){return function(e){return A=t.value,"true"===t.getAttribute(D)&&A===t.getAttribute(V)&&M.inArray(C,e.keyCode)?(e.preventDefault&&e.preventDefault(),!1):void 0}}function d(t){return function(){n(t,A),""===t.value&&(t.blur(),M.moveCaret(t,0))}}function g(t){return function(){t===r()&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)&&M.moveCaret(t,0)}}function v(t){return function(){i(t)}}function p(t){t.form&&(T=t.form,"string"==typeof T&&(T=document.getElementById(T)),T.getAttribute(U)||(M.addEventListener(T,"submit",v(T)),T.setAttribute(U,"true"))),M.addEventListener(t,"focus",o(t)),M.addEventListener(t,"blur",c(t)),m&&(M.addEventListener(t,"keydown",s(t)),M.addEventListener(t,"keyup",d(t)),M.addEventListener(t,"click",g(t))),t.setAttribute(j,"true"),t.setAttribute(V,x),(m||t!==r())&&a(t)}var b,f,m,h,A,y,E,x,L,T,N,S,w,B=["text","search","url","tel","email","password","number","textarea"],C=[27,33,34,35,36,37,38,39,40,8,46],k="#ccc",I="placeholdersjs",R=RegExp("(?:^|\\s)"+I+"(?!\\S)"),V="data-placeholder-value",D="data-placeholder-active",P="data-placeholder-type",U="data-placeholder-submit",j="data-placeholder-bound",q="data-placeholder-focus",z="data-placeholder-live",F="data-placeholder-maxlength",G=document.createElement("input"),H=document.getElementsByTagName("head")[0],J=document.documentElement,K=t.Placeholders,M=K.Utils;if(K.nativeSupport=void 0!==G.placeholder,!K.nativeSupport){for(b=document.getElementsByTagName("input"),f=document.getElementsByTagName("textarea"),m="false"===J.getAttribute(q),h="false"!==J.getAttribute(z),y=document.createElement("style"),y.type="text/css",E=document.createTextNode("."+I+" { color:"+k+"; }"),y.styleSheet?y.styleSheet.cssText=E.nodeValue:y.appendChild(E),H.insertBefore(y,H.firstChild),w=0,S=b.length+f.length;S>w;w++)N=b.length>w?b[w]:f[w-b.length],x=N.attributes.placeholder,x&&(x=x.nodeValue,x&&M.inArray(B,N.type)&&p(N));L=setInterval(function(){for(w=0,S=b.length+f.length;S>w;w++)N=b.length>w?b[w]:f[w-b.length],x=N.attributes.placeholder,x?(x=x.nodeValue,x&&M.inArray(B,N.type)&&(N.getAttribute(j)||p(N),(x!==N.getAttribute(V)||"password"===N.type&&!N.getAttribute(P))&&("password"===N.type&&!N.getAttribute(P)&&M.changeType(N,"text")&&N.setAttribute(P,"password"),N.value===N.getAttribute(V)&&(N.value=x),N.setAttribute(V,x)))):N.getAttribute(D)&&(n(N),N.removeAttribute(V));h||clearInterval(L)},100)}M.addEventListener(t,"beforeunload",function(){K.disable()}),K.disable=K.nativeSupport?e:i,K.enable=K.nativeSupport?e:l}(this);
 
 
+
+
 			$( document ).ready(function() {
-				    $("#email-form").submit(function(e){
-				        e.preventDefault();
-				    });
-
-				    $("#email").focus(function() {
-		  				$('#email').removeClass('input-error');
-					  	$('.subscribe-caption').html('(Få varsel på e-post når nettmagsinet lanseres)');
-					  	$('.subscribe-caption').removeClass('error-message');
-					});
-
-
+				    
+				   	
 				  setTimeout(function(){
-
-				    // Fixed HTML rendering bug
+			   	
+				   	// Fixed HTML rendering bug
 				    var parent = $('.instagram-cell').parent()
 				    var html = $('.instagram-cell').parent().html()
 				    $('.instagram-cell').parent().html('');
@@ -161,150 +154,162 @@
 				    var html = $('.subscribe-table').html()
 				    $('.subscribe-table').html('');
 				    $('.subscribe-table').html(html);
-				    
 
-				  },0);
+
+
+
+
+
+			    $("#email-form").submit(function(e){
+			        e.preventDefault();
+			    });
+
+			    $("#email").focus(function() {
+	  				$('#email').removeClass('input-error');
+				  	$('.subscribe-caption').html('(Få varsel på e-post når nettmagsinet lanseres)');
+				  	$('.subscribe-caption').removeClass('error-message');
+				});
+
+
+
+
+
+				$('#subscribe-button').click(function(){
+
+					// alert("SEND");
+
+					$('#email').blur();
+
+					var email = $('#email').val();
+
+					// Simple email validation
+					var is_valid_email = /^.+@.+\..+$/.test(email);
+
+				  if(!is_valid_email){
+				  	$('#email').addClass('input-error');
+				  	$('.subscribe-caption').html('Oops! Har du skrevet riktig e-post adresse?');
+				  	$('.subscribe-caption').addClass('error-message');
+					$('.placeholdersjs').val('Skriv inn din epost');
+				  	return;
+				  }
+					
+
+					$('#email').focus();
+					$('#email').blur();
+
+				var opts = {
+				  lines: 13, // The number of lines to draw
+				  length: 6, // The length of each line
+				  width: 2, // The line thickness
+				  radius: 6, // The radius of the inner circle
+				  corners: 1, // Corner roundness (0..1)
+				  rotate: 0, // The rotation offset
+				  direction: 1, // 1: clockwise, -1: counterclockwise
+				  color: '#fff', // #rgb or #rrggbb or array of colors
+				  speed: 1, // Rounds per second
+				  trail: 60, // Afterglow percentage
+				  shadow: false, // Whether to render a shadow
+				  hwaccel: false, // Whether to use hardware acceleration
+				  className: 'spinner', // The CSS class to assign to the spinner
+				  zIndex: 2e9, // The z-index (defaults to 2000000000)
+				  top: '50%', // Top position relative to parent
+				  left: '50%' // Left position relative to parent
+				};
+
+				$('#subscribe-button').addClass('no-text');
+				$('#subscribe-button').addClass('ajax-spinner');
+
+				var target = document.getElementById('subscribe-button');
+				var spinner = new Spinner(opts).spin(target);
+
+					jQuery.ajax({
+					  type: 'POST',
+					  url: '/wp-admin/admin-ajax.php',
+					  data: {
+					  action: 'RegisterSubscriber',
+					  email: $('#email').val()
+					  },
+
+					  success: function(data, textStatus, XMLHttpRequest){
+					  	// alert("SUCCESS");
+						  // jQuery("#test-div1").html('');
+						  // jQuery("#test-div1").append(data);
+						  // alert(data);
+						  
+						  $('#subscribe-button').removeClass('ajax-spinner');
+						  spinner.stop();
+
+						  if( $("html").hasClass("lt-ie10") ){
+		                  	$('.subscribe-input-cell').hide();
+		                  	$('.subscribe-button-cell').hide();
+		                  	$('.subscribe-success-message-ie9').show();
+						  }
+						  else{
+							  $('.subscribe-success-message').fadeIn(250);
+						  }
+
+						  setTimeout(function(){
+
+						  if( $("html").hasClass("lt-ie10") ){
+		                  	$('.subscribe-input-cell').show();
+		                  	$('.subscribe-button-cell').show();
+		                  	$('.subscribe-success-message').hide();
+		                  	$('.subscribe-success-message-ie9').hide();
+
+						    $('#email').focus();
+							$('#email').blur();
+						  }
+						  else{
+							  $('.subscribe-success-message').fadeOut(250);
+						  }
+
+						  $('#subscribe-button').removeClass('no-text');
+						  $('#email').val('');
+						  $('.placeholdersjs').val('Skriv inn din epost');
+
+						  },3000);
+
+						  /*
+							$('#overlay-content-container').fadeOut(200, function(){
+							  	spinner.stop();
+							  	var jsonData = jQuery.parseJSON(data);
+							  	$('#overlay-content-container').html(jsonData["members"]);
+								setTimeout(function(){
+									$('#overlay-content-container').fadeIn(500);
+									centerContent();
+								}, 50);
+							});
+							*/
+
+						  /*
+						  var jsonData = jQuery.parseJSON(data);
+						  $(jsonData["members"]).appendTo('#member-list-thumbnail').hide().fadeIn(500);
+
+						  if(jsonData["more"] == 0){
+						  	$('#spinner-container').hide();
+						  }
+						  else{
+							$('#spinner-container').waypoint(function(direction) {
+
+								APP.members.loadMore();
+								$('#spinner-container').waypoint('destroy');
+							
+							}, { offset: 'bottom-in-view' })
+
+						  }
+						  */
+					  },
+					  error: function(MLHttpRequest, textStatus, errorThrown){
+					  	// alert(errorThrown);
+					  }
+					  });
+
+				})
+
+				  },100);
 
 			});
 
-
-
-
-
-
-
-		$('#subscribe-button').click(function(){
-
-			// alert("SEND");
-
-			$('#email').blur();
-
-			var email = $('#email').val();
-
-			// Simple email validation
-			var is_valid_email = /^.+@.+\..+$/.test(email);
-
-		  if(!is_valid_email){
-		  	$('#email').addClass('input-error');
-		  	$('.subscribe-caption').html('Oops! Har du skrevet riktig e-post adresse?');
-		  	$('.subscribe-caption').addClass('error-message');
-			$('.placeholdersjs').val('Skriv inn din epost');
-		  	return;
-		  }
-			
-
-			$('#email').focus();
-			$('#email').blur();
-
-		var opts = {
-		  lines: 13, // The number of lines to draw
-		  length: 6, // The length of each line
-		  width: 2, // The line thickness
-		  radius: 6, // The radius of the inner circle
-		  corners: 1, // Corner roundness (0..1)
-		  rotate: 0, // The rotation offset
-		  direction: 1, // 1: clockwise, -1: counterclockwise
-		  color: '#fff', // #rgb or #rrggbb or array of colors
-		  speed: 1, // Rounds per second
-		  trail: 60, // Afterglow percentage
-		  shadow: false, // Whether to render a shadow
-		  hwaccel: false, // Whether to use hardware acceleration
-		  className: 'spinner', // The CSS class to assign to the spinner
-		  zIndex: 2e9, // The z-index (defaults to 2000000000)
-		  top: '50%', // Top position relative to parent
-		  left: '50%' // Left position relative to parent
-		};
-
-		$('#subscribe-button').addClass('no-text');
-		$('#subscribe-button').addClass('ajax-spinner');
-
-		var target = document.getElementById('subscribe-button');
-		var spinner = new Spinner(opts).spin(target);
-
-			jQuery.ajax({
-			  type: 'POST',
-			  url: '/wp-admin/admin-ajax.php',
-			  data: {
-			  action: 'RegisterSubscriber',
-			  email: $('#email').val()
-			  },
-
-			  success: function(data, textStatus, XMLHttpRequest){
-			  	// alert("SUCCESS");
-				  // jQuery("#test-div1").html('');
-				  // jQuery("#test-div1").append(data);
-				  // alert(data);
-				  
-				  $('#subscribe-button').removeClass('ajax-spinner');
-				  spinner.stop();
-
-				  if( $("html").hasClass("lt-ie10") ){
-                  	$('.subscribe-input-cell').hide();
-                  	$('.subscribe-button-cell').hide();
-                  	$('.subscribe-success-message-ie9').show();
-				  }
-				  else{
-					  $('.subscribe-success-message').fadeIn(250);
-				  }
-
-				  setTimeout(function(){
-
-				  if( $("html").hasClass("lt-ie10") ){
-                  	$('.subscribe-input-cell').show();
-                  	$('.subscribe-button-cell').show();
-                  	$('.subscribe-success-message').hide();
-                  	$('.subscribe-success-message-ie9').hide();
-
-				    $('#email').focus();
-					$('#email').blur();
-				  }
-				  else{
-					  $('.subscribe-success-message').fadeOut(250);
-				  }
-
-				  $('#subscribe-button').removeClass('no-text');
-				  $('#email').val('');
-				  $('.placeholdersjs').val('Skriv inn din epost');
-
-				  },3000);
-
-				  /*
-					$('#overlay-content-container').fadeOut(200, function(){
-					  	spinner.stop();
-					  	var jsonData = jQuery.parseJSON(data);
-					  	$('#overlay-content-container').html(jsonData["members"]);
-						setTimeout(function(){
-							$('#overlay-content-container').fadeIn(500);
-							centerContent();
-						}, 50);
-					});
-					*/
-
-				  /*
-				  var jsonData = jQuery.parseJSON(data);
-				  $(jsonData["members"]).appendTo('#member-list-thumbnail').hide().fadeIn(500);
-
-				  if(jsonData["more"] == 0){
-				  	$('#spinner-container').hide();
-				  }
-				  else{
-					$('#spinner-container').waypoint(function(direction) {
-
-						APP.members.loadMore();
-						$('#spinner-container').waypoint('destroy');
-					
-					}, { offset: 'bottom-in-view' })
-
-				  }
-				  */
-			  },
-			  error: function(MLHttpRequest, textStatus, errorThrown){
-			  	// alert(errorThrown);
-			  }
-			  });
-
-
-		})
 
 		var videos = new Array('<?php echo $videos[0]['video']; ?>', '<?php echo $videos[1]['video']; ?>', '<?php echo $videos[2]['video']; ?>', '<?php echo $videos[3]['video']; ?>', '<?php echo $videos[4]['video']; ?>');
 
