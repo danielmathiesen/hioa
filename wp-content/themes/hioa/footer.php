6b49f1da2e09c1a003b1c99f5ed3a1828ca766b6
Daniel Mathiesen <daniel.mathiesen@gmail.com>
2014-08-04 15:27:04 +0200

    Added Google analytics code

--- wp-content/themes/hioa/footer.php
+++ wp-content/themes/hioa/footer.php
@@ -14,14 +14,15 @@
   		<script src="<?php bloginfo('template_directory'); ?>/js/spin.min.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-52406016-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
 
 
 
@@ -268,36 +269,6 @@
 						  $('.placeholdersjs').val('Skriv inn din epost');
 
 						  },3000);
-
-						  /*
-							$('#overlay-content-container').fadeOut(200, function(){
-							  	spinner.stop();
-							  	var jsonData = jQuery.parseJSON(data);
-							  	$('#overlay-content-container').html(jsonData["members"]);
-								setTimeout(function(){
-									$('#overlay-content-container').fadeIn(500);
-									centerContent();
-								}, 50);
-							});
-							*/
-
-						  /*
-						  var jsonData = jQuery.parseJSON(data);
-						  $(jsonData["members"]).appendTo('#member-list-thumbnail').hide().fadeIn(500);
-
-						  if(jsonData["more"] == 0){
-						  	$('#spinner-container').hide();
-						  }
-						  else{
-							$('#spinner-container').waypoint(function(direction) {
-
-								APP.members.loadMore();
-								$('#spinner-container').waypoint('destroy');
-							
-							}, { offset: 'bottom-in-view' })
-
-						  }
-						  */
 					  },
 					  error: function(MLHttpRequest, textStatus, errorThrown){
 					  	// alert(errorThrown);
